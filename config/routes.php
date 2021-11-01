02a22467045e850de6bc003c6a066190fb9ea42e
Nicolas Amado <nimado70@hotmail.com>
2021-11-01 16:10:58 +0000

    bug fixes

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,8 @@ use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection();
 $routes->add('home', new Route('/', ['controller' => 'WorldOfWonders\Controller\ProductController', 'method' => 'list']));
+// $routes->add('addtobasket', new Route('/{productId}', ['controller' => 'WorldOfWonders\Controller\ProductController', 'method' => 'addtobasket']));
+
 $routes->add('basket', new Route('/basket', ['controller' => 'WorldOfWonders\Controller\BasketController', 'method' => 'basket']));
 $routes->add('delete', new Route('/basket/{productId}', ['controller' => 'WorldOfWonders\Controller\BasketController', 'method' => 'deleteItem']));
 $routes->add('checkout', new Route('/checkout', ['controller' => 'WorldOfWonders\Controller\CheckoutController', 'method' => 'checkout']));
